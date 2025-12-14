05a692ba7c36f3fb9c4ac0832548855870d56931
AndreyQuantum <zelentsov1254@gmail.com>
2025-12-14 20:47:26 +0100

    + RESULT_FILE_NAME to settings

--- algorithms_exam/src/configs/app.py
+++ algorithms_exam/src/configs/app.py
@@ -1,3 +1,5 @@
+from os import PathLike
+
 from dynaconf import Dynaconf
 from pydantic import BaseModel
 
@@ -5,11 +7,13 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     SIMILARITY_THRESHOLD: float
     USE_PREPROCESSING: bool
+    RESULT_FILE_NAME: PathLike = "duplicates.json"
 
 env_settings = Dynaconf(settings_file=["settings.yml"])
 
 
 settings = Settings(
     SIMILARITY_THRESHOLD=env_settings["SIMILARITY_THRESHOLD"],
-    USE_PREPROCESSING=env_settings["USE_PREPROCESSING"]
+    USE_PREPROCESSING=env_settings["USE_PREPROCESSING"],
+    RESULT_FILE_NAME = env_settings["RESULT_FILE_NAME"],
 )

ef6376068308d6a7efe7a94323768afd5475cec4
AndreyQuantum <zelentsov1254@gmail.com>
2025-12-15 01:30:44 +0100

    Update lint workflow and refactor main.py to initialize FileService logic

--- algorithms_exam/src/main.py
+++ algorithms_exam/src/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from algorithms_exam.src.service.file_service import FileService
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    file_service = FileService()
+    catalog = file_service.read_catalog()
+    new_items = file_service.read_new_items()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    main()
